b3690f76560c7bea89b07def1a1c629cec50cacb
DarrelA <darrelaiscoding@gmail.com>
2022-05-27 16:36:14 +0800

    Fixed bug when logging in via Google OAuth without logging in using noarmal login first

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -4,7 +4,8 @@ const notFoundMiddleware = (req, res, next) =>
   next(new HttpError(`Not Found - ${req.originalUrl}`, 404));
 
 const errorMiddleware = (err, req, res, next) => {
-  if (res.headerSent) return next(err);
+  if (err.name === 'InternalOAuthError') return; // prevents cannot fetch user error when logging in
+  if (res.headersSent) return next(err);
   res.status(err.code || 500).json({
     message: err.message || 'Something went wrong!',
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
